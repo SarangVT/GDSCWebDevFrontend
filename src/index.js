8b76d1a8013ae927fe9c267a3697cd0deeab45b4
SarangVT <svdthakare@gmail.com>
2025-02-05 20:39:28 +0530

    HashRouter

--- src/index.js
+++ src/index.js
@@ -2,13 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import {BrowserRouter} from "react-router-dom";
+import {HashRouter} from "react-router-dom";
 import { UserContextProvider } from './Context/userData';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <UserContextProvider>
-    <BrowserRouter>
+    <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </UserContextProvider>
 );
